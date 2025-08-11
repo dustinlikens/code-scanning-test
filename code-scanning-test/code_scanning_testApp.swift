d6aac731964ab8199991e4f2f4788617b8e5800e
Dustin Likens <dustinlikens@icloud>
2025-08-11 08:17:45 -0700

    init

---
+++ code-scanning-test/code_scanning_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_scanning_testApp.swift
+//  code-scanning-test
+//
+//  Created by dustinlikens on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct code_scanning_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
